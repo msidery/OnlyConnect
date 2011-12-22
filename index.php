4178d0f5283e780669f7b30bf558b3d391f61759
Martin <mjs10>
2011-12-22 20:17:06 +0000

    stuck the sql stuff in index

--- index.php
+++ index.php
@@ -7,7 +7,48 @@
 </head>
 <body>
 
-<?php ?>
+<?php
+
+define('DB_SERVER', 'localhost');
+define('DB_NAME', 'only_connect');
+define('DB_USER', 'root');
+define('DB_PASS', 's3cur3');
+
+// some of the code i used for badminton website. does database stuff. figure it out :P
+
+
+// open database connection
+$con = mysql_connect(DB_SERVER, DB_USER, DB_PASS);
+if (!$con) {
+	die('Could not connect: '.mysql_error());
+}
+
+mysql_select_db(DB_NAME, $con);
+
+$result = mysql_query("SELECT * FROM contacts ORDER BY contact_id");
+$count = 0;
+while($row = mysql_fetch_array($result))
+{
+	$result2 = mysql_query("SELECT * FROM contacts WHERE contact_id=".$count);
+	$num = mysql_num_rows ($result2);
+	echo '<div class="content"><p><strong>'.$row['title'].'</strong><br/>';
+	echo $row['name'].'<br/>';
+	echo '<em><a href="mailto: '.$row['email'].'">'.$row['email'].'</a></em>';
+	while ($num > 1) {
+		$row = mysql_fetch_array($result);
+		echo $row['name'].'<br/>';
+		echo '<em><a href="mailto: '.$row['email'].'">'.$row['email'].'</a></em>';
+		$num--;
+	}
+	echo "</p>\n</div>";
+	$count++;
+}
+
+// close connection
+mysql_close($con);
+include('temp.php');
+?>
+
 
 </body>
 </html>
